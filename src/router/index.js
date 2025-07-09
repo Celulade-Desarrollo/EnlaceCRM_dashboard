b65ed22529e9e8d6c3895ea527085ed135565a6d
Sebastian Zapata <sebastian.zapata@grupocnet.com>
2025-07-09 13:28:28 -0500

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -84,7 +84,6 @@ const routes = [
     component: () => import("../views/PantallaFacturasDView.vue"),
     meta: { requiresAuth: true },
   },
-  }
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
