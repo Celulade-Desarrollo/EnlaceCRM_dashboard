696275101c3964b09b6cd9efdfe1739348b6afea
Sebastian Zapata <sebastian.zapata@grupocnet.com>
2025-07-15 10:20:14 -0500

    pago pse button

--- src/views/Pantalla1View.vue
+++ src/views/Pantalla1View.vue
@@ -83,7 +83,16 @@ const goToPantalla5 = () => {
         :deudaTotal="estadoCuenta.deudaTotal"
         @abonar="goToPantalla5"
       />
-      <div>
+      <div class="bg-white w-full h-40 rounded-xl flex flex-col items-center relative justify-start pt-3">
+        <h2 class="w-full text-center font-bold mb-2">¿Cómo quieres pagar?</h2>
+        <a href="https://portalpagos.payty.com/PortalPagosPayty/WEB/?codigoConvenio=112878"
+          class=" no-underline flex items-center justify-between bg-gray-100 rounded-lg shadow w-72 h-20 px-4 mt-4"
+        >
+          <span class=" no-underline flex flex-col text-left font-bold text-gray-700 text-lg leading-tight">
+            Pago<br />Digital via PSE
+          </span>
+          <img src="../../public/PSELOGO.png" class="w-16 h-16" />
+        </a>
       </div>
     </section>
   </motion.div>
